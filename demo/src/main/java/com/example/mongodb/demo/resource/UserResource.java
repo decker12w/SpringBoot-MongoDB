5ca36c1a592b583e39789be4c3d57d60e0555549
josemaia <josemaia.comp@gmail.com>
2024-02-15 02:53:39 -0300

    first-commit

---
+++ demo/src/main/java/com/example/mongodb/demo/resource/UserResource.java
@@ -0,0 +1,26 @@
+package com.example.mongodb.demo.resource;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.mongodb.demo.domain.User;
+import com.example.mongodb.demo.service.UserService;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @Autowired
+    private UserService service;
+
+    @GetMapping
+    public ResponseEntity<List<User>> findAll() {
+        List<User> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+}
